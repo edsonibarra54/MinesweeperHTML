ae8658b35158021d81a4136e58e7d5bb1fbade86
Edson Ibarra <edsonibarra54@gmail.com>
2023-09-27 00:43:19 -0600

    The board is initilized with ceros

--- js/functionality.js
+++ js/functionality.js
@@ -1,3 +1,29 @@
-function flipBox(posI, posJ){
+var tam = 10;
+var board;
 
+function begin(){
+    //Creating board
+    board = new Array(tam);
+    for(var i = 0 ; i < tam ; i++){
+        board[i] = new Array(tam);
+    }
+
+    //Initializing board in cero
+    for(var i = 0 ; i < tam ; i++){
+        for(var j = 0 ; j < tam ; j++){
+            board[i][j] = 0;
+        }
+    }
+
+    printBoard();
+}
+
+function printBoard(){
+    for(var i = 0 ; i < tam ; i++){
+        console.log(board[i])
+    }
+}
+
+function flipBox(posI, posJ){
+    
 }
